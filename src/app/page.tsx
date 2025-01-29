dcc15c81e7e124892a6e7622d64baa4d0fd3b3f6
britcruise9 <britjcruise@gmail.com>
2025-01-28 22:01:02 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import ColorMatcher from '@/components/color-matcher';
 const Home = () => {
   return (
     <div>
-      <header className="bg-[#5B92D1] py-8 mb-8">
-        <div className="max-w-3xl mx-auto px-4 text-center">
-          <h1 className="text-4xl font-bold text-white font-['Playfair Display', 'serif']">
-            CHROMA HOME
-          </h1>
-          <p className="text-lg text-[#D5E0EF]">Shop your color</p>
-        </div>
+       <header className="flex h-36 mb-8">
+      <div className="w-1/7 bg-[#D1CBAA]"></div>
+      <div className="w-1/7 bg-[#F0EFE7]"></div>
+      <div className="w-1/7 bg-[#B0A589]"></div>
+      <div className="w-1/7 bg-[#CBBEA5]"></div>
+      <div className="w-1/7 bg-[#E3DECD]"></div>
+      <div className="w-1/7 bg-[#D1CBAA]"></div>
+      <div className="flex-1 flex flex-col items-center justify-center text-white">
+        <h1 className="text-4xl font-bold font-['Playfair Display', 'serif']">CHROMA HOME</h1>
+        <p className="text-lg">Shop your color</p>
+      </div>
       </header>
       <main>
         <ColorMatcher />
