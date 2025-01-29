9c95202a2bca62e75a780bc7c95b07bbd5d26960
britcruise9 <britjcruise@gmail.com>
2025-01-28 21:49:38 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ import ColorMatcher from '@/components/color-matcher';
 const Home = () => {
   return (
     <div>
+      <header className="bg-[#5B92D1] py-8 mb-8">
+        <div className="max-w-3xl mx-auto px-4 text-center">
+          <h1 className="text-4xl font-bold text-white font-['Playfair Display', 'serif']">
+            CHROMA HOME
+          </h1>
+          <p className="text-lg text-[#D5E0EF]">Shop your color</p>
+        </div>
+      </header>
       <main>
         <ColorMatcher />
       </main>
