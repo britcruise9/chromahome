e2f4751d400eda75a5f9c5c7a465d53ead391bdb
britcruise9 <britjcruise@gmail.com>
2025-01-28 20:30:40 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    domains: ['fakestoreapi.com']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*'
+      }
+    ];
+  }
+}
+
+module.exports = nextConfig
