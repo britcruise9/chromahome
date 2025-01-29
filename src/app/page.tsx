20dfa53885e9acfdfecc3f263597159ab42cc574
britcruise9 <britjcruise@gmail.com>
2025-01-28 21:26:08 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import ColorMatcher from '@/components/color-matcher';
 
-const UpdatedHeader = () => {
+const Home = () => {
   return (
-    <header className="bg-[#5B92D1] py-8 mb-8">
-      <div className="max-w-3xl mx-auto px-4 text-center">
-        <h1 className="text-4xl font-bold text-white font-['Playfair Display', 'serif']">CHROMA HOME</h1>
-        <p className="text-lg text-[#D5E0EF]">Shop your color</p>
-      </div>
-    </header>
+    <div>
+      <header className="bg-[#5B92D1] py-8 mb-8">
+        <div className="max-w-3xl mx-auto px-4 text-center">
+          <h1 className="text-4xl font-bold text-white font-['Playfair Display', 'serif']">
+            CHROMA HOME
+          </h1>
+          <p className="text-lg text-[#D5E0EF]">Shop your color</p>
+        </div>
+      </header>
+      <main>
+        <ColorMatcher />
+      </main>
+    </div>
   );
 };
 
-export default UpdatedHeader;
+export default Home;
