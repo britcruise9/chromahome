bf49c353e9125f3774a1efeaa5a9ed53233905ba
britcruise9 <britjcruise@gmail.com>
2025-01-29 19:50:57 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <header className="bg-[#5B92D1] py-8 mb-8">
         <div className="max-w-3xl mx-auto px-4 text-center">
           <h1 className="text-4xl font-bold text-white font-['Playfair Display', 'serif']">
-            CHROMA HOME
+            CHROMA
           </h1>
           <p className="text-lg text-[#D5E0EF]">Shop your color</p>
         </div>
