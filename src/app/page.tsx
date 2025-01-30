0bc592573c2cd0c233685223d882cde9ddb1bc28
britcruise9 <britjcruise@gmail.com>
2025-01-29 20:05:03 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-import React from 'react';
-import ColorMatcher from '@/components/color-matcher';
+import ModernUploader from '@/components/modern-uploader';
 
-const Home = () => {
-  return (
-    <div>
-      <header className="bg-[#5B92D1] py-8 mb-8">
-        <div className="max-w-3xl mx-auto px-4 text-center">
-          <h1 className="text-4xl font-bold text-white font-['Playfair Display', 'serif']">
-            CHROMA
-          </h1>
-          <p className="text-lg text-[#D5E0EF]">Shop your color</p>
-        </div>
-      </header>
-      <main>
-        <ColorMatcher />
-      </main>
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <ModernUploader />;
+}
