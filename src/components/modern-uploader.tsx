3659dce187150616645921286acf7ddcfe07e1cd
britcruise9 <britjcruise@gmail.com>
2025-02-11 10:54:01 -0500

    Update modern-uploader.tsx

--- src/components/modern-uploader.tsx
+++ src/components/modern-uploader.tsx
@@ -223,7 +223,7 @@ const ModernUploader = () => {
         <div className="mt-12">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {products.map((product) => (
-              
+              <a
                 key={product.id}
                 href={product.affiliateLink}
                 target="_blank"
